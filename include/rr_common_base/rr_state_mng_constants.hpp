e281a833c6e3fdc8f26c1d0f982faa7b8dd98d6c
Aaron Spiteri <azzmosphere@gmail.com>
2025-11-06 18:03:53 +1100

    Added constants for state messages

--- include/rr_common_base/rr_state_mng_constants.hpp
+++ include/rr_common_base/rr_state_mng_constants.hpp
@@ -1,31 +1,32 @@
 #ifndef RR_STATE_MNG_CONSTANTS_HPP
 #define RR_STATE_MNG_CONSTANTS_HPP
 
+#include <string>
 /**
  * The following constants are used for communication with the state manager service.
  */
 namespace rr_constants
 {
     // define state battery request
-    #define STATE_BAT_REQ "state_bat_req"
+    const std::string STATE_BAT_REQ = "state_bat_req";
 
     // define state GPS request
-    #define STATE_GPS_REQ "state_gps_req"
+    const std::string STATE_GPS_REQ = "state_gps_req";
 
     // define state image request
-    #define STATE_IMG_REQ "state_img_req"
+    const std::string STATE_IMG_REQ = "state_img_req";
 
     // define state IMU request
-    #define STATE_IMU_REQ "state_imu_req"
+    const std::string STATE_IMU_REQ = "state_imu_req";
 
     // define state joystick request
-    #define STATE_JOY_REQ "state_joy_req"
+    const std::string STATE_JOY_REQ = "state_joy_req";
 
     // define state range request
-    #define STATE_RNG_REQ "state_rng_req"
+    const std::string STATE_RNG_REQ = "state_rng_req";
 
     // define state response
-    #define STATE_RESPONSE "state_response"
-}
+    const std::string STATE_RESPONSE = "state_response";
+} // namespace rr_constants
 
 #endif
