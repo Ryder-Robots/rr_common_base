edaa72dff4c5341e6d8e5a64d515f28f8f31bf4f
Aaron Spiteri <azzmosphere@gmail.com>
2025-11-21 18:21:00 +1100

    added copywrite notifications

--- include/rr_common_base/rr_node_plugin_iface.hpp
+++ include/rr_common_base/rr_node_plugin_iface.hpp
@@ -17,38 +17,3 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
-
-#ifndef RR_STATE_MNG_CONSTANTS_HPP
-#define RR_STATE_MNG_CONSTANTS_HPP
-
-#include <string>
-/**
- * The following constants are used for communication with the state manager service.
- */
-namespace rr_constants_state_mgr
-{
-    // define state battery request
-    const std::string STATE_BAT_REQ = "state_bat_req";
-
-    // define state GPS request
-    const std::string STATE_GPS_REQ = "state_gps_req";
-
-    // define state image request
-    const std::string STATE_IMG_REQ = "state_img_req";
-
-    // define state IMU request
-    const std::string STATE_IMU_REQ = "state_imu_req";
-
-    // define state joystick request
-    const std::string STATE_JOY_REQ = "state_joy_req";
-
-    // define navigation request, this is an automous request
-    const std::string STATE_NAV_REQ = "state_nav_req";
-
-    // define state range request
-    const std::string STATE_RNG_REQ = "state_rng_req";
-    
-} // namespace rr_constants
-
-#endif
