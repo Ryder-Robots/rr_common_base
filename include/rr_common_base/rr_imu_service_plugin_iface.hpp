3918adfe139877b0004f66388ce6c3c520c6678c
Aaron Spiteri <azzmosphere@gmail.com>
2025-12-09 14:45:46 +1100

    added interface for IMU plugin

---
+++ include/rr_common_base/rr_imu_service_plugin_iface.hpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2025 Ryder Robots
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "rr_common_base/rr_service_plugin_iface.hpp"
+#include "rr_interfaces/srv/monitor_imu_action.hpp"
+
+namespace rrobots
+{
+    namespace interfaces
+    {
+        /**
+         * @class RRImuServicePluginIface
+         * @brief provides plugin interface for IMU events.
+         * 
+         * Concrete implementations populate MonitorImuAction::Result.imu from
+         * flight controllers or embedded sensors. Uses header.frame_id for
+         * link targeting and uuid for request correlation.
+         */
+        class RRImuServicePluginIface : public RRServicePluginIface<rr_interfaces::srv::MonitorImuAction>
+        {
+        };
+    } // namespace interfaces
+} // namespace rrobots
